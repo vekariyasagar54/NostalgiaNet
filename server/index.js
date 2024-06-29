21fcc0d8d12bc1eac9f8466bbb473638f1308251
vekariyasagar54 <113746663+vekariyasagar54@users.noreply.github.com>
2024-06-29 11:22:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors(
     {
-        origin : [""],
+        origin : ["http://localhost:3000"],
         method : ["POST" , "GET"],
         credentials : true
     }
