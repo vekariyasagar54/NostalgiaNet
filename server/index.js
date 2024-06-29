7d0119935405aca1fe693a83ce8ef15895c75639
vekariyasagar54 <113746663+vekariyasagar54@users.noreply.github.com>
2024-06-29 11:48:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors(
     {
-        origin : ["http://localhost:3000"],
+        origin : ["https://nostalgia-net-frontend.vercel.app"],
         method : ["POST" , "GET"],
         credentials : true
     }
