98a0cf7cd43a751e90caaa6281382d75e7f09a0f
Sagar vekariya <vekariyasagar2011@gmail.com>
2024-06-29 09:45:23 +0530

    Initial commit of NostalgiaNet project

---
+++ client/src/components/Auth/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider} from 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyDV57ionylJybB-nVnyYko7FZHKwEOBq_o",
+    authDomain: "nostalgianet-5f1ad.firebaseapp.com",
+    projectId: "nostalgianet-5f1ad",
+    storageBucket: "nostalgianet-5f1ad.appspot.com",
+    messagingSenderId: "285115491910",
+    appId: "1:285115491910:web:8d5c5e879d349dca5e0c47",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+
+export const provider = new GoogleAuthProvider();
