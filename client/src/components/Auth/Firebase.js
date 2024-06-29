5afcf0dba263666ed67dbb681723547045203c10
vekariyasagar54 <113746663+vekariyasagar54@users.noreply.github.com>
2024-06-29 11:53:47 +0530

    Update Firebase.js

--- client/src/components/Auth/Firebase.js
+++ client/src/components/Auth/Firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider} from 'firebase/auth';
 const firebaseConfig = {
-    apiKey: "AIzaSyDV57ionylJybB-nVnyYko7FZHKwEOBq_o",
-    authDomain: "nostalgianet-5f1ad.firebaseapp.com",
-    projectId: "nostalgianet-5f1ad",
-    storageBucket: "nostalgianet-5f1ad.appspot.com",
-    messagingSenderId: "285115491910",
-    appId: "1:285115491910:web:8d5c5e879d349dca5e0c47",
+  apiKey: "AIzaSyDV57ionylJybB-nVnyYko7FZHKwEOBq_o",
+  authDomain: "nostalgianet-5f1ad.firebaseapp.com",
+  projectId: "nostalgianet-5f1ad",
+  storageBucket: "nostalgianet-5f1ad.appspot.com",
+  messagingSenderId: "285115491910",
+  appId: "1:285115491910:web:8d5c5e879d349dca5e0c47",
+  measurementId: "G-ECBR9GCKDM"
 };
 
 const app = initializeApp(firebaseConfig);
