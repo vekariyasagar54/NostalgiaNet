7b6b902a6490dc84e7559850492a597fbbe7eb36
vekariyasagar54 <113746663+vekariyasagar54@users.noreply.github.com>
2024-06-30 14:32:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,11 +10,21 @@ import userRoutes from './routes/users.js';
 const app = express();
 dotenv.config();
 
-
 // Middlewares
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+
+const corsOptions = {
+    origin: "https://nostalgia-net-frontend.vercel.app", // Frontend domain
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.get('/', (req, res) => {
     res.send(`<center><h1>Hello from server. </h1><h2> Use /posts and /users endpoint</h2></center>`);
@@ -22,8 +32,7 @@ app.get('/', (req, res) => {
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.CONNECTION_URL, {
@@ -35,7 +44,3 @@ mongoose
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
     })
     .catch((error) => console.log(error.message));
-
-
-module.exports = app;
-
